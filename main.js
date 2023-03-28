b5ca657655cfa39ace1bd99a6a2d1c1c6e568fab
Francisco Corrêa <franciscopcorrea@gmail.com>
2023-03-28 09:05:41 -0300

    Commit Inicial

---
+++ main.js
@@ -0,0 +1,29 @@
+const form = document.getElementById('number-check');
+
+
+function checkSecondNumber(number1, number2) {
+    const check =  number2 >= number1;
+    return check;
+}
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    const number1 = parseFloat(document.getElementById('number-1').value);
+    const number2 = parseFloat(document.getElementById('number-2').value);
+
+    const messageerror = 'Número menor que o anterior';
+
+    valor_1 = checkSecondNumber(number1, number2);
+
+    if (!valor_1) {
+        const espacomensagem = document.querySelector('.message-error');
+        espacomensagem.innerHTML = messageerror;
+        espacomensagem.style.display = 'block';
+
+    } else {
+        document.querySelector('.message-error').style.display = 'none';
+    }
+
+
+})
